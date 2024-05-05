8286b542ce4dfdf61c1fc12086ddc77b64bb0a17
Mageri <roma.nikulin.2007@mail.ru>
2024-05-06 01:20:03 +0300

    Test

---
+++ Assets/Scripts/AI/Doody.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Doody : MonoBehaviour
+{
+    private Transform playerTransform;
+    [SerializeField] private float maxDistance;
+    [SerializeField] private float minDistance;
+    [SerializeField] private float maxDistanceDelta = .01f;
+    [SerializeField] private float maxHeight = 0.5f;
+
+    private void Awake()
+    {
+        playerTransform = Player.Player.instancePlayer.transform;
+    }
+
+
+    private void Update()
+    {
+        Vector3 dir = playerTransform.position - transform.position;
+        Quaternion lookRot = Quaternion.LookRotation(dir);
+        lookRot.x = 0; lookRot.z = 0;
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Mathf.Clamp01(3.0f * Time.maximumDeltaTime));
+
+        if (Physics.Raycast(transform.position, Vector3.down,out var hit, maxHeight))
+        {
+            if (TryGetComponent(out Rigidbody rb))
+            {
+                rb.constraints = RigidbodyConstraints.FreezePositionY;
+            }
+        }
+        else if (TryGetComponent(out Rigidbody rb) && rb.constraints == RigidbodyConstraints.FreezePositionY)
+        {
+            rb.constraints = RigidbodyConstraints.None;
+            //transform.position = new Vector3(transform.position.x,maxHeight,transform.position.z);
+        }
+        
+        if ((playerTransform.position - transform.position).magnitude > maxDistance)
+        {
+            Vector3 target = new Vector3(playerTransform.position.x,transform.position.y,playerTransform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, target, maxDistanceDelta);
+        }
+
+        if ((playerTransform.position - transform.position).magnitude < minDistance)
+        {
+            Vector3 target = new Vector3(-playerTransform.position.x,transform.position.y,-playerTransform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, target, maxDistanceDelta);
+        }
+        
+    }
+
+}
---
+++ Assets/Scripts/AI/IKFootSolver.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class IKFootSolver : MonoBehaviour
+{
+    [SerializeField] private Transform body;
+    [SerializeField] private float footSpacing;
+    [SerializeField] private float stepDistance;
+    [SerializeField] private float stepHeight;
+    
+    [SerializeField] private float lerp = 0;
+    [SerializeField] private float speed;
+    private Vector3 newPosition;
+    private Vector3 currentPosition;
+    private Vector3 oldPosition;
+    private void Update()
+    {
+        transform.position = currentPosition;
+        Ray ray = new Ray(body.position + (body.right * footSpacing), Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit info, 10))
+        {
+            if (Vector3.Distance(newPosition, info.point) > stepDistance)
+            {
+                lerp = 0;
+                newPosition = info.point;
+            }
+            //transform.position = info.point;
+        }
+        if (lerp < 1)
+        {
+            Vector3 footPosition = Vector3.Lerp(oldPosition, newPosition, lerp);
+            footPosition.y += Mathf.Sin(lerp * Mathf.PI) * stepHeight;
+
+            currentPosition = footPosition;
+            lerp += Time.deltaTime * speed;
+        }
+        else
+        {
+            oldPosition = newPosition;
+        }
+    }
+}
